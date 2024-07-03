2331c451d67c22fb7b7f4078ea2fef3b172c6306
vishnu130902 <vishnu1309n@gmail.com>
2024-07-03 21:54:45 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,70 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React , as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jan 2024 - April 2024",
+    role: "Software Developer Intern",
+    company: "IIITDM Jabalpur",
+    description: `Collaborated with a 5-member team to create the Primary Health Center module app for FUSION IIIT, achieving reduction in patient wait times and enhancing healthcare access.`,
+    technologies: ["Flutter", "Dart", "Django", "mongoDB"],
+  },
+  {
+    year: "Aug 2023 - Sep 2023",
+    role: "Web Developer Intern",
+    company: "Adobe",
+    description: `Created a temperature converter website using HTML, CSS, and JavaScript, demonstrating proficiency in front-end web development technologies.Also,Designed and implemented a prototype homepage for Netflix, showcasing skills in web design and user interface.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    year: "May 2023 - July 2023",
+    role: "Cyber Security Intern",
+    company: "HCLTech",
+    description: `Carried out detailed and systematic risk evaluations, scrutinizing the security and integrity of the current information systems infrastructure to identify vulnerabilities and potential threats. Enhanced my knowledge of the latest security threats by thoroughly exploring blogs and online publications,
+staying up-to-date with current trends in cybersecurity`,
+    technologies: [],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Happy Estate",
+    image: project1,
+    description:
+      "The project is a real estate e-commerce website for property listings (rent/sale) with CRUD operations. It includes detailed property info and user authentication, featuring Google sign-in for secure access.",
+    technologies: ["CSS", "React", "Node", "MongoDB","Express"],
+  },
+  {
+    title: "Footware Store",
+    image: project2,
+    description:
+      "Developed a responsive footwear store website using Next.js and Tailwind CSS, ensuring smooth performance across all devices. Enhanced functionality and user engagement through effective design strategies.",
+    technologies: ["HTML", "TailwindCSS", "Next", "JavaScript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "TailwindCSS", "React", "JavaScript"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["CSS","React","Node", "MongoDB","Express"],
+  },
+];
+
+export const CONTACT = {
+  address: "C-20, Paramount GolfForeste, Greater Noida, Uttar Pradesh, India - 201308",
+  phoneNo: "+91 9643530152  ",
+  email: "vishnu1309n@gmail.com",
+};
